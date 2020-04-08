e0c0537ed20dcd66b56d63fdf058c18253187649
UnaLai <qqq323618@gmail.com>
2020-04-08 17:10:51 +0800

    [feat]:add beforeAll and afterAll

--- __test__/setupTeardown.test.js
+++ __test__/setupTeardown.test.js
@@ -1,1 +1,17 @@
 const trigger = require('../js/trigger');
+
+beforeAll(() => {
+  trigger("beforeAll");
+});
+
+afterAll(() => {
+  trigger("afterAll");
+});
+
+test('test sommething one', () => {
+  expect(true).toBe(true);
+});
+
+test('test sommething two', () => {
+  expect(true).toBe(true);
+});
