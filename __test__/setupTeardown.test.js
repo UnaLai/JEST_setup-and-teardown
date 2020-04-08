d9b66682515aa3145bdf29f42a89e797db31e424
UnaLai <qqq323618@gmail.com>
2020-04-08 20:54:55 +0800

    [feat]:add trigger in describe

--- __test__/setupTeardown.test.js
+++ __test__/setupTeardown.test.js
@@ -8,20 +8,58 @@ const trigger = require('../js/trigger');
 //   trigger("afterAll");
 // });
 
-beforeEach(() => {
-  trigger("beforeEach");
+// beforeEach(() => {
+//   trigger("beforeEach");
+// });
+
+// afterEach(() => {
+//   trigger("afterEach");
+// });
+
+// test('test something one', () => {
+//   console.log("test something one")
+//   expect(true).toBe(true);
+// });
+
+// test('test something two', () => {
+//   console.log("test something two")
+//   expect(true).toBe(true);
+// });
+
+// 加入 scope 考量
+
+beforeAll(() => {
+  trigger("beforeAll outside");
 });
 
-afterEach(() => {
-  trigger("afterEach");
+afterAll(() => {
+  trigger("afterAll outside");
 });
 
-test('test something one', () => {
-  console.log("test something one")
+test('test something outside', () => {
+  console.log("test something outside")
   expect(true).toBe(true);
 });
 
-test('test something two', () => {
-  console.log("test something two")
-  expect(true).toBe(true);
-});
+// describe('describe scope', () => {
+//   beforeAll(() => {
+//     trigger("beforeAll inside");
+//   });
+  
+//   afterAll(() => {
+//     trigger("afterAll inside");
+//   });
+
+//   test('test something inside', () => {
+//     console.log('test something inside')
+//     expect(true).toBe(true);
+//   });
+// })
+
+describe('describe scope', () => {
+  trigger("trigger in describe");
+  test('test something inside', () => {
+    console.log('test something inside')
+    expect(true).toBe(true);
+  });
+})
